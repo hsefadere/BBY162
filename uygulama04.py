3a941de17e4814348f7219dd7518ec66e4be4d1d
Harun Sefa DERE <48154854+hsefadere@users.noreply.github.com>
2019-04-24 14:35:09 +0300

    Add files via upload

---
+++ uygulama04.py
@@ -0,0 +1,34 @@
+metin = "Açık bilim, araştırma çıktılarına ve süreçlerine herkesin serbestçe erişmesini, bunların ortak kullanımını, dağıtımını ve üretimini kolaylaştıran bilim uygulamasıdır."
+
+metinden = metin[:20]
+print(metinden)
+
+print("********************************************")
+##################################################
+
+liste = ["Açık Bilim", "Açık Erişim", "Açık Lisans", "Açık Eğitim", "Açık Veri", "Açık Kültür"]
+
+for x in liste:
+    print(x)
+
+print("********************************************")
+###################################################
+
+
+sozluk = {"elma" : "Ağaçta yetişen bir tür meyve" , "salatalık" : "Fidan üzerinde büyüyen bir tür sebze" }
+
+kelime= input("Aradığınız kelimeyi lütfen buraya yazınız:")
+
+
+while True:
+    if kelime == "elma":
+        print("Aradığınız kelime sözlükte bulunmaktadır")
+        print(sozluk["elma"])
+        break
+    elif kelime == "salatalık":
+        print("Aradığınız kelime sözlükte bulunmaktadır")
+        print(sozluk["salatalık"])
+        break
+    else:
+        print("Aradığınız kelime sözlükte bulunmamaktadır")
+        break
